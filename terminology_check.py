a731cc02a110179e6f25ac882d069b14557da0d8
Jon Lee <39420293+4ka0@users.noreply.github.com>
2020-03-15 12:39:01 +0000

    Update terminology checker

--- terminology_check.py
+++ terminology_check.py
@@ -15,45 +15,48 @@ Features:
 	String comparisons
 
 To execute:
-	python3 terminology_check.py tmxfile txtfile
+	python3 terminology_check.py glossary.txt translation.tmx
 '''
+
 
 import sys
 import xml.etree.ElementTree as ET
 
-if verify.user_input_check(sys.argv):
-        segments = gather.gather_segments(sys.argv[1])
 
-
-def user_input_check(argv):
+def user_input_check(user_input):
     '''
     Function for validating user input entered at the command line.
+    Expected input:
+    	python3 terminology_check.py glossary.txt translation.tmx
     Aspects checked:
-        Two arguments should have been entered.
-        The second argument should be a tmx file.
-        The existence of the actual tmx file is checked in gather.py
-            (The “easier to ask for forgiveness than permission” (EAFP)
-            approach is used to avoid a race condition.)
+        3 arguments should have been entered
+        1st argument should be a python script
+        2nd argument should be a txt file (glossary)
+        3rd argument should be a tmx file (translation)
     '''
     input_verified = True
 
-    # If two arguments have not been entered
-    if len(argv) != 2:
+    # Check if 3 arguments have been input
+    if len(user_input) != 3:
         input_verified = False
-        print(Fore.RED + 'Incorrect number of arguments entered.')
 
-    # If two arguments have been entered
     else:
-        file = argv[1]
+        glossary_file = user_input[1]
+        translation_file = user_input[2]
 
-        # If the second argument is not a tmx file
-        if not file.lower().endswith('.tmx'):
+        # Check if 2nd argument is a txt file
+        if not glossary_file.lower().endswith('.txt'):
             input_verified = False
-            print(Fore.RED + 'Incorrect file type. Only tmx files accepted.')
 
+        # Check if 3rd argument is a tmx file
+        if not translation_file.lower().endswith('.tmx'):
+            input_verified = False
+
+    # Error message   
     if not input_verified:
-        print(Fore.RED + 'Please try again using the following format.')
-        print(Fore.CYAN + 'python3 checker.py yourfile.tmx')
+        print(' Incorrect input.\n'
+        	  ' Please try again using the following format.\n'
+        	  ' python3 terminology_check.py glossary.txt translation.tmx')
 
     return input_verified
 
@@ -62,31 +65,44 @@ def get_terminology():
 	'''
 	Function for extracting terminology from user-specified txt file.
 	'''
-	pass
+	
+	terminology = []
+
+	# Get data from file, populate list
+	# line by line or all at once?
+
+	with open(filename) as f:
+    	content = f.readlines()
+		# Remove whitespace characters such as \n at the end of each line
+		content = [line.rstrip() for line in content]
+
+	return terminology
 
 
 def get_translation():
 	'''
 	Function for extracting translation from user-specified tmx file.
 	'''
-	pass
+	translation = []
+	return translation
 
 
 def check_translation():
 	'''
 	Function for checking terminology against the translation.
 	'''
-	
+	results = []
 	'''
 	Parse through tmx file segment by segment
 		In each segment, check if Jap text contains word in Jap list
 			If yes, count number of instances in Jap text
-					check corresponding Eng word is in Eng text same number of instances
+					check corresponding Eng word is in Eng text at 
+					the same number of instances
 						If yes, no problem
 						If not, report error
 			If not, no problem
 	'''
-	pass
+	return results
 
 
 def output_results():
@@ -97,10 +113,12 @@ def output_results():
 
 
 def main():
-	if user_input_check(sys.argv):
-		get_terminology()
-		get_translation()
-		check_translation()
+	user_input = sys.argv
+	if user_input_check(user_input):
+		terminology = get_terminology(user_input[3])
+		translation = get_translation(user_input[2])
+		results = check_translation(terminology, translation)
+		output_results(results)
 		
 		
 if __name__ == "__main__":
