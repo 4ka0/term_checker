3855d529825b336df9d3e74eab63deb28c5a1fbc
Jon Lee <39420293+4ka0@users.noreply.github.com>
2020-04-16 22:00:25 +0100

    General cleaning up

--- term_checker.py
+++ term_checker.py
@@ -6,10 +6,17 @@ Script to check whether project-specific terminology is being used
 in a translation (tmx file).
 
 Terminology is assumed to be listed in a tab-delimited file (txt file)
-with the following format: Source<tab>Target
+with the following format:
+    source_term <tab> target_term
+
+If multiple target terms exist for a given source term, it is assumed that
+these are entered on separate lines rather than all on the same line, e.g.:
+    source_term <tab> target_term_1
+    source_term <tab> target_term_2
+    source_term <tab> target_term_3
 
 To execute:
-python3 term_checker.py glossary.txt translation.tmx
+    python3 term_checker.py glossary.txt translation.tmx
 '''
 
 
@@ -22,10 +29,10 @@ class Segment():
     Used to create objects for each source-target segment extracted
     from a tmx file.
     '''
-    def __init__(self, source_text, target_text, missing):
+    def __init__(self, source_text, target_text, missing_terms):
         self.source_text = source_text
         self.target_text = target_text
-        self.missing = missing  # Missing terms, if found
+        self.missing_terms = missing_terms
 
 
 def user_input_check(user_input):
@@ -41,7 +48,7 @@ def user_input_check(user_input):
     '''
     input_verified = True
 
-    # Check if 3 arguments have been input
+    # Check if 3 arguments have been input.
     if len(user_input) != 3:
         input_verified = False
 
@@ -49,19 +56,19 @@ def user_input_check(user_input):
         glossary_file = user_input[1]
         translation_file = user_input[2]
 
-        # Check if 2nd argument is a txt file
+        # Check if 2nd argument is a txt file.
         if not glossary_file.lower().endswith('.txt'):
             input_verified = False
 
-        # Check if 3rd argument is a tmx file
+        # Check if 3rd argument is a tmx file.
         if not translation_file.lower().endswith('.tmx'):
             input_verified = False
 
-    # Error message   
+    # Error message.
     if not input_verified:
-        print(' Incorrect input.\n'
-              ' Please try again using the following format.\n'
-              ' python3 terminology_check.py glossary.txt translation.tmx')
+        print('\nIncorrect input.\n'
+              'Please try again using the following format.\n'
+              'python3 terminology_check.py glossary.txt translation.tmx\n')
 
     return input_verified
 
@@ -81,8 +88,8 @@ def get_terminology(glossary_file):
 
 def clean_lines(terminology):
     '''
-    Function to clean enties in terminology list and also to remove duplicate 
-    entries. 
+    Function to clean entries in a terminology list and also to remove 
+    duplicate entries. Specifically:
     (1) Removes surrounding whitespace chars from each line (including '\n')
     (2) Removes '*' chars from the start of each line (I have these in some of 
         my client glossaries).
@@ -92,12 +99,12 @@ def clean_lines(terminology):
     return terminology
 
 
-def entry_check(terminology):
+def format_check(terminology):
     '''
     Function to check whether there are two entries on each line. In other
     words, whether each line contains two substrings (source and target text)
     delimited by a single tab character. Lines not formatted this way are
-    passed over.
+    ignored.
     '''
     formatted_terminology = []
     for entry in terminology:
@@ -133,20 +140,15 @@ def group_terminology(terminology):
         source_term = split_terms[0].lower()
         target_term = split_terms[1].lower()
 
-        # Check if source term already appears as a key
+        # If source term already appears as a key.
         if source_term in grouped_terminology:
-            
-            # Add the target term to the list corresponding to the source term
-            # Get the current list of target terms for the source term
-            translations = grouped_terminology[source_term]
-            # Append the new target term to the list
-            translations.append(target_term)
-            # Assign the new larger list to the source term
-            grouped_terminology[source_term] = translations
+            target_terms = grouped_terminology[source_term]
+            target_terms.append(target_term)
+            grouped_terminology[source_term] = target_terms
 
         else:            
-            # Add new entry with the key being the source term and 
-            # the value being a list containing the single target term
+            # Add new entry with the source term as the key and
+            # a list containing the single target term as the value.
             grouped_terminology[source_term] = [target_term]
             
     return grouped_terminology
@@ -167,6 +169,7 @@ def get_translation(translation_file):
         for node in tmx_file.unit_iter():
             source_text = node.source
             target_text = node.target
+            # {} below is 'missing_terms'
             segment = Segment(source_text, target_text, {})
             translation.append(segment)
 
@@ -186,18 +189,18 @@ def check_translation(terminology, translation):
             if (not segment.source_text.isspace() and 
                 not segment.target_text.isspace()):
         
-                # Check if any source terminology is in the source text
-                # term here is the keys in the termonology dict
-                for term in terminology:
-                    if term in segment.source_text:
+                # Check if any source terminology is in the source text.
+                # 'source_term' here refers to keys in the terminology dict.
+                for entry in terminology:
+                    if entry in segment.source_text:
 
                         # Check if any corresponding target term appears 
                         # in the target text.
                         found = any(elem in segment.target_text.lower()
-                                     for elem in terminology[term])    
+                                    for elem in terminology[entry])    
                         
                         if found == False:
-                            segment.missing[term] = terminology[term]
+                            segment.missing_terms[entry] = terminology[entry]
 
     return translation
 
@@ -209,29 +212,29 @@ def output_results(translation):
     errors_found = False
 
     for segment in translation:
-        if segment.missing:
+        if segment.missing_terms:
             
             errors_found = True
             print('\n')
 
-            for source_term in segment.missing:
-                print('\'' + source_term + '\' should be translated as', end=' ')
+            for source in segment.missing_terms:
+                print('\'' + source + '\' should be translated as', end=' ')
 
-                # Get number of target terms.
-                target_num = len(segment.missing[source_term])
+                # Get the number of target terms.
+                target_num = len(segment.missing_terms[source])
                 counter = 0
 
-                for target_term in segment.missing[source_term]:
+                for target in segment.missing_terms[source]:
                     counter += 1
                     # Second to last element.
                     if counter == target_num - 1:
-                        print('\'' + target_term + '\'', end=', or ')
+                        print('\'' + target + '\'', end=', or ')
                     # Last element.
                     elif counter == target_num:  
-                        print('\'' + target_term + '\'', end=' ')
+                        print('\'' + target + '\'', end=' ')
                     # Any other element.
                     else:
-                        print('\'' + target_term + '\'', end=', ')
+                        print('\'' + target + '\'', end=', ')
             
             print('\nSource text:')
             print(segment.source_text)
@@ -247,7 +250,7 @@ def main():
     if user_input_check(user_input):
         terminology = get_terminology(user_input[1])
         terminology = clean_lines(terminology)
-        terminology = entry_check(terminology)
+        terminology = format_check(terminology)
         terminology = remove_duplicates(terminology)
         terminology = group_terminology(terminology)
         translation = get_translation(user_input[2])
