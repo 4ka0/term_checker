c5d2e593615b584957b474fa5d1bf22254b39009
Jon Lee <39420293+4ka0@users.noreply.github.com>
2020-04-16 22:27:12 +0100

    Test update

--- tests/unit_test.py
+++ tests/unit_test.py
@@ -7,6 +7,10 @@ from .. import term_checker
 from ..term_checker import Segment
 
 
+GLOSSARY_FILE = 'tests/test_glossary.txt'
+TRANSLATION_FILE = 'tests/test_translation.tmx'
+
+
 def test_constructor():
     s = Segment('なお、正孔輸送層12は、NiO、（またはMoO3）等の無機材料を...',
                 'Moreover, the positive  hole transport layers 12 may...',
@@ -46,16 +50,72 @@ def test_get_terminology():
                 '*従来技術	related art\n',
                 '*解決	address\n',
                 '*解決	solve\n',
-                '*解決	solution']
-    glossary_file = 'tests/test_glossary.txt'
-    terminology = term_checker.get_terminology(glossary_file)
+                '*解決	solution\n',
+                '*装置	device	apparatus\n',
+                '*特許	patent	patent']
+    terminology = term_checker.get_terminology(GLOSSARY_FILE)
     assert terminology == expected
 
 
-def test_clean_terminology():
+def test_clean_lines():
     expected = ['技術分野	Technical Field',
                 '背景技術	Related Art', 
                 '発明の概要	Summary', 
+                '発明の概要	Summary', 
+                '発明が解決しようとする課題	Problem to be Solved by the Invention',
+                '課題を解決するための手段	Means for Solving the Problem',
+                '図面の簡単な説明	Brief Description of the Drawings',
+                '発明を実施するための形態	Detailed Description',
+                '特許請求の範囲	What is Claimed is:',
+                '特許請求の範囲	patent claims',
+                '要約書	Abstract',
+                '要約書	Abstract',
+                '要約書	Abstract',
+                '実施形態	exemplary embodiment',
+                '実施の形態	exemplary embodiment',
+                '実施例	example',
+                '従来技術	related art',
+                '解決	address',
+                '解決	solve',
+                '解決	solution',
+                '装置	device	apparatus',
+                '特許	patent	patent']
+    terminology = term_checker.get_terminology(GLOSSARY_FILE)
+    terminology = term_checker.clean_lines(terminology)
+    assert terminology == expected
+
+
+def test_format_check():
+    expected = ['技術分野	Technical Field',
+                '背景技術	Related Art', 
+                '発明の概要	Summary', 
+                '発明の概要	Summary', 
+                '発明が解決しようとする課題	Problem to be Solved by the Invention',
+                '課題を解決するための手段	Means for Solving the Problem',
+                '図面の簡単な説明	Brief Description of the Drawings',
+                '発明を実施するための形態	Detailed Description',
+                '特許請求の範囲	What is Claimed is:',
+                '特許請求の範囲	patent claims',
+                '要約書	Abstract',
+                '要約書	Abstract',
+                '要約書	Abstract',
+                '実施形態	exemplary embodiment',
+                '実施の形態	exemplary embodiment',
+                '実施例	example',
+                '従来技術	related art',
+                '解決	address',
+                '解決	solve',
+                '解決	solution']
+    terminology = term_checker.get_terminology(GLOSSARY_FILE)
+    terminology = term_checker.clean_lines(terminology)
+    terminology = term_checker.format_check(terminology)
+    assert terminology == expected
+
+
+def test_remove_duplicates():
+    expected = ['技術分野	Technical Field',
+                '背景技術	Related Art', 
+                '発明の概要	Summary',
                 '発明が解決しようとする課題	Problem to be Solved by the Invention',
                 '課題を解決するための手段	Means for Solving the Problem',
                 '図面の簡単な説明	Brief Description of the Drawings',
@@ -70,14 +130,18 @@ def test_clean_terminology():
                 '解決	address',
                 '解決	solve',
                 '解決	solution']
-    glossary_file = 'tests/test_glossary.txt'
-    terminology = term_checker.get_terminology(glossary_file)
-    terminology = term_checker.clean_terminology(terminology)
+    terminology = term_checker.get_terminology(GLOSSARY_FILE)
+    terminology = term_checker.clean_lines(terminology)
+    terminology = term_checker.format_check(terminology)
+    terminology = term_checker.remove_duplicates(terminology)
     assert terminology == expected
 
 
+def test_group_terminology():
+    pass
+
+
 def test_get_translation():
-    translation_file = 'tests/test_translation.tmx'
     expected = [('[図1]...を示す断面模式図である。', 
                  'Fig. 1 is a cross-sectional schematic view depicting ...'),
                 ('[図2]...を説明する図である。', 
@@ -92,7 +156,7 @@ def test_get_translation():
                  'Fig. 6 is a drawing depicting ...'),
                 ('[図7]...を示す平面模式図である。', 
                  'Fig. 7 is a plan schematic view depicting ...')]
-    segments = term_checker.get_translation(translation_file)
+    segments = term_checker.get_translation(TRANSLATION_FILE)
     translation = []
     for segment in segments:
         translation.append((segment.source_text, segment.target_text))
