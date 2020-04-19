4a56bebc32527dddad0f77b8ae0a9c3abaed013b
Jon Lee <39420293+4ka0@users.noreply.github.com>
2020-04-19 16:08:48 +0100

    Updated unit tests, added integration test

--- tests/unit_test.py
+++ tests/unit_test.py
@@ -60,114 +60,108 @@ def test_get_terminology():
 
 
 def test_clean_lines():
+    input = [' 技術分野	Technical Field  \n',
+             '  背景技術	Related Art  \n',
+             '   発明の概要	Summary \n',
+             '    発明の概要	Summary \n',
+             '      課題	Problem  \n',
+             ' *手段	Means \n',
+             '   *図面の簡単な説明	Brief Description of the Drawings \n',
+             '*発明を実施するための形態	Detailed Description   \n',
+             '*特許請求の範囲	What is Claimed is:	\n',
+             '*特許請求の範囲	patent claims	\n']
     expected = ['技術分野	Technical Field',
                 '背景技術	Related Art',
                 '発明の概要	Summary',
                 '発明の概要	Summary',
-                '発明が解決しようとする課題	Problem to be Solved by the Invention',
-                '課題を解決するための手段	Means for Solving the Problem',
+                '課題	Problem',
+                '手段	Means',
                 '図面の簡単な説明	Brief Description of the Drawings',
                 '発明を実施するための形態	Detailed Description',
                 '特許請求の範囲	What is Claimed is:',
-                '特許請求の範囲	patent claims',
-                '要約書	Abstract',
-                '要約書	Abstract',
-                '要約書	Abstract',
-                '実施形態	exemplary embodiment',
-                '実施の形態	exemplary embodiment',
-                '実施例	example',
-                '従来技術	related art',
-                '解決	address',
-                '解決	solve',
-                '解決	solution',
-                '装置	device	apparatus',
-                '特許	patent	patent',
-                '断面模式図	cross-sectional schematic view',
-                '平面模式図	plan schematic view']
-    terminology = term_checker.get_terminology(GLOSSARY_FILE)
-    terminology = term_checker.clean_lines(terminology)
-    assert terminology == expected
+                '特許請求の範囲	patent claims']
+    output = term_checker.clean_lines(input)
+    assert output == expected
 
 
 def test_format_check():
+    input = ['技術分野	Technical Field',
+             '背景技術	Related Art',
+             '発明の概要	Summary',
+             '発明の概要	Summary',
+             '課題	Problem',
+             '手段, Means',
+             '図面の簡単な説明',
+             '発明を実施するための形態,Detailed Description',
+             '特許請求の範囲	What is Claimed is:	patent claims',
+             '特許請求の範囲/patent claims/claims']
     expected = ['技術分野	Technical Field',
                 '背景技術	Related Art',
                 '発明の概要	Summary',
                 '発明の概要	Summary',
-                '発明が解決しようとする課題	Problem to be Solved by the Invention',
-                '課題を解決するための手段	Means for Solving the Problem',
-                '図面の簡単な説明	Brief Description of the Drawings',
-                '発明を実施するための形態	Detailed Description',
-                '特許請求の範囲	What is Claimed is:',
-                '特許請求の範囲	patent claims',
-                '要約書	Abstract',
-                '要約書	Abstract',
-                '要約書	Abstract',
-                '実施形態	exemplary embodiment',
-                '実施の形態	exemplary embodiment',
-                '実施例	example',
-                '従来技術	related art',
-                '解決	address',
-                '解決	solve',
-                '解決	solution',
-                '断面模式図	cross-sectional schematic view',
-                '平面模式図	plan schematic view']
-    terminology = term_checker.get_terminology(GLOSSARY_FILE)
-    terminology = term_checker.clean_lines(terminology)
-    terminology = term_checker.format_check(terminology)
-    assert terminology == expected
+                '課題	Problem']
+    ouput = term_checker.format_check(input)
+    assert ouput == expected
 
 
 def test_remove_duplicates():
+    input = ['技術分野	Technical Field',
+             '背景技術	Related Art',
+             '発明の概要	Summary',
+             '発明の概要	Summary',
+             '発明の概要	Summary',
+             '課題	Problem',
+             '手段	Means',
+             '図面の簡単な説明	Brief Description of the Drawings',
+             '図面の簡単な説明	Brief Description of the Drawings',
+             '発明を実施するための形態	Detailed Description',
+             '特許請求の範囲	What is Claimed is:',
+             '特許請求の範囲	patent claims']
     expected = ['技術分野	Technical Field',
                 '背景技術	Related Art',
                 '発明の概要	Summary',
-                '発明が解決しようとする課題	Problem to be Solved by the Invention',
-                '課題を解決するための手段	Means for Solving the Problem',
+                '課題	Problem',
+                '手段	Means',
                 '図面の簡単な説明	Brief Description of the Drawings',
                 '発明を実施するための形態	Detailed Description',
                 '特許請求の範囲	What is Claimed is:',
-                '特許請求の範囲	patent claims',
-                '要約書	Abstract',
-                '実施形態	exemplary embodiment',
-                '実施の形態	exemplary embodiment',
-                '実施例	example',
-                '従来技術	related art',
-                '解決	address',
-                '解決	solve',
-                '解決	solution',
-                '断面模式図	cross-sectional schematic view',
-                '平面模式図	plan schematic view']
-    terminology = term_checker.get_terminology(GLOSSARY_FILE)
-    terminology = term_checker.clean_lines(terminology)
-    terminology = term_checker.format_check(terminology)
-    terminology = term_checker.remove_duplicates(terminology)
-    assert terminology == expected
+                '特許請求の範囲	patent claims']
+    output = term_checker.remove_duplicates(input)
+    assert output == expected
 
 
 def test_group_terminology():
+    input = ['技術分野	Technical Field',
+             '発明の概要	Summary',
+             '特許請求の範囲	What is Claimed is:',
+             '特許請求の範囲	patent claims',
+             '要約書	Abstract',
+             '実施形態	exemplary embodiment',
+             '実施の形態	exemplary embodiment',
+             '解決	address',
+             '解決	solve',
+             '解決	solution',
+             '装置	device',
+             '装置	apparatus',
+             'つまり	that is',
+             'つまり	in other words',
+             'つまり	namely',
+             'つまり	specifically',
+             '断面模式図	cross-sectional schematic view',
+             '平面模式図	plan schematic view']
     expected = {'技術分野': ['Technical Field'],
-                '背景技術': ['Related Art'],
                 '発明の概要': ['Summary'],
-                '発明が解決しようとする課題': ['Problem to be Solved by the Invention'],
-                '課題を解決するための手段': ['Means for Solving the Problem'],
-                '図面の簡単な説明': ['Brief Description of the Drawings'],
-                '発明を実施するための形態': ['Detailed Description'],
                 '特許請求の範囲': ['What is Claimed is:', 'patent claims'],
                 '要約書': ['Abstract'],
                 '実施形態': ['exemplary embodiment'],
                 '実施の形態': ['exemplary embodiment'],
-                '実施例': ['example'],
-                '従来技術': ['related art'],
                 '解決': ['address', 'solve', 'solution'],
+                '装置': ['device', 'apparatus'],
+                'つまり': ['that is', 'in other words', 'namely', 'specifically'],
                 '断面模式図': ['cross-sectional schematic view'],
                 '平面模式図': ['plan schematic view']}
-    terminology = term_checker.get_terminology(GLOSSARY_FILE)
-    terminology = term_checker.clean_lines(terminology)
-    terminology = term_checker.format_check(terminology)
-    terminology = term_checker.remove_duplicates(terminology)
-    terminology = term_checker.group_terminology(terminology)
-    assert terminology == expected
+    output = term_checker.group_terminology(input)
+    assert output == expected
 
 
 def test_get_translation():
@@ -186,13 +180,23 @@ def test_get_translation():
                 ('[図7]...を示す平面模式図である。',
                  'Fig. 7 is a plan schematic depicting ...')]
     segments = term_checker.get_translation(TRANSLATION_FILE)
-    translation = []
+    output = []
     for segment in segments:
-        translation.append((segment.source_text, segment.target_text))
-    assert translation == expected
+        output.append((segment.source_text, segment.target_text))
+    assert output == expected
 
 
 def test_check_translation():
+    terminology = {'技術分野': ['Technical Field'],
+                   '発明の概要': ['Summary'],
+                   '特許請求の範囲': ['What is Claimed is:', 'patent claims'],
+                   '要約書': ['Abstract'],
+                   '実施形態': ['exemplary embodiment'],
+                   '実施の形態': ['exemplary embodiment'],
+                   '解決': ['address', 'solve', 'solution'],
+                   '装置': ['device', 'apparatus'],
+                   '断面模式図': ['cross-sectional schematic view'],
+                   '平面模式図': ['plan schematic view']}
     expected = [('[図1]...を示す断面模式図である。',
                  'Fig. 1 is a schematic view depicting ...',
                  {'断面模式図': ['cross-sectional schematic view']}),
@@ -215,23 +219,13 @@ def test_check_translation():
                  'Fig. 7 is a plan schematic depicting ...',
                  {'平面模式図': ['plan schematic view']})]
 
-    # Build translation
+    # Get and check translation
     translation = term_checker.get_translation(TRANSLATION_FILE)
+    checked_trans = term_checker.check_translation(terminology, translation)
 
-    # Build terminology
-    terminology = term_checker.get_terminology(GLOSSARY_FILE)
-    terminology = term_checker.clean_lines(terminology)
-    terminology = term_checker.format_check(terminology)
-    terminology = term_checker.remove_duplicates(terminology)
-    terminology = term_checker.group_terminology(terminology)
+    # Extract content from Segment objects for assertion comparison
+    output = []
+    for seg in checked_trans:
+        output.append((seg.source_text, seg.target_text, seg.missing_terms))
 
-    # Check translation
-    translation = term_checker.check_translation(terminology, translation)
-
-    # Extract content from Segment objects for assert comparison
-    readable_translation = []
-    for segment in translation:
-        readable_translation.append((segment.source_text, segment.target_text,
-                                     segment.missing_terms))
-
-    assert readable_translation == expected
+    assert output == expected
